3bfc432b43b31e02ff6406a4ed624e63351c3d15
maksim_bezditko <mb.email.for.studying@gmail.com>
2022-12-16 08:10:56 +0200

    f

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const result = document.querySelector(".result");
 
 function recursively() {
   function fibonacci(n) {
-    if (n === 1) return 0;
+    if (n === 1 || n == 0) return 0;
     if (n === 2 || n === 3) return 1;
 
     return fibonacci(n - 1) + fibonacci(n - 2);
